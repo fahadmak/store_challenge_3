b9af671d1db7846902b6c4653af42208c6c30257
faraqhan <f.faraqhan@gmail.com>
2018-10-29 00:52:24 +0300

    add create_user endpoint

--- app/views/user.py
+++ app/views/user.py
@@ -1,5 +1,22 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify, request
+
+from app.error_handler import InvalidUsage
+from app.validate import validate_user, empty
+from app.models.database import Database
+from flask import current_app as app
+
 
 user = Blueprint("user", __name__)
 
+
+@user.route("/api/v1/auth/signup", methods=["POST"])
+def create_user():
+    """A method adds product instance to products list"""
+    db = Database(app.config['DATABASE_URI'])
+    data = request.json
+    name = data.get("name")
+    username = data.get("username")
+    password = data.get("password")
+    db.create_user(name, username, password)
+    return jsonify({'message': f'{username} has successfully been added to staff'}), 200
 
