da4644332064e1c8d3954deebaaa1faf408de607
faraqhan <f.faraqhan@gmail.com>
2018-10-28 21:08:24 +0300

    add Database instance in app.py file

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
+import os
+
+from app.models.database import Database
 from app import create_app
 
-
 app = create_app("development")
-
+db = Database(app.config['DATABASE_URI'])
 if __name__ == '__main__':
     app.run()
