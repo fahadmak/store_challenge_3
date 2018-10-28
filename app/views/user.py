1e4f3fe5ed1c2af8189db39844b1cf59b751c2b0
faraqhan <f.faraqhan@gmail.com>
2018-10-29 00:54:10 +0300

    add validations for endpoint [#161533073]

--- app/views/user.py
+++ app/views/user.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, jsonify, request
 
 from app.error_handler import InvalidUsage
+from app.validate import validate_user, empty
 from app.models.database import Database
 from flask import current_app as app
 
@@ -12,10 +13,21 @@ user = Blueprint("user", __name__)
 def create_user():
     """A method adds product instance to products list"""
     db = Database(app.config['DATABASE_URI'])
+    if request.content_type != "application/json":
+        raise InvalidUsage("Invalid content type", 400)
     data = request.json
     name = data.get("name")
     username = data.get("username")
     password = data.get("password")
+    check = empty(name, username, password)
+    if check:
+        raise InvalidUsage(check, 400)
+    invalid = validate_user(name, username, password)
+    if invalid:
+        raise InvalidUsage(invalid, 400)
+    found = db.find('users', 'username', username)
+    if found:
+        raise InvalidUsage(f"{username} already exists", 400)
     db.create_user(name, username, password)
     return jsonify({'message': f'{username} has successfully been added to staff'}), 200
 
