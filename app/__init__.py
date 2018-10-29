263deb098599ebf8c5853b0327db0572356f7e98
faraqhan <f.faraqhan@gmail.com>
2018-10-29 12:32:40 +0300

    update requirements.txt

--- app/__init__.py
+++ app/__init__.py
@@ -8,10 +8,8 @@ def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(app_config[config_name])
 
-    from app.views.product import product as product_blueprint
     from app.views.user import user as user_blueprint
 
-    app.register_blueprint(product_blueprint)
     app.register_blueprint(user_blueprint)
 
     return app
