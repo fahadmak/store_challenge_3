6c90597b592f6befe1dc9e18e1b66676219097bd
faraqhan <f.faraqhan@gmail.com>
2018-10-29 11:36:32 +0300

    refactor code [#161533073]

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+
 from config import app_config
 
 
@@ -8,9 +9,11 @@ def create_app(config_name):
     app.config.from_object(app_config[config_name])
 
     from app.views.product import product as product_blueprint
-    from app.views.sale import sale as sale_blueprint
+    from app.views.user import user as user_blueprint
 
     app.register_blueprint(product_blueprint)
-    app.register_blueprint(sale_blueprint)
+    app.register_blueprint(user_blueprint)
 
     return app
+
+
