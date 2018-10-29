9dfc49fa58d6f9baf67fdd909c878e95fdbe1da8
faraqhan <f.faraqhan@gmail.com>
2018-10-29 11:59:17 +0300

    remove db object from app.py [#161533073]

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
-import os
-
-from app.models.database import Database
 from app import create_app
 
 app = create_app("development")
-db = Database(app.config['DATABASE_URI'])
+
+
 if __name__ == '__main__':
     app.run()
