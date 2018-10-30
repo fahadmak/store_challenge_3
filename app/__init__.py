a6e2f7c9a687ab440f3a636f1bda64e2f8804944
faraqhan <f.faraqhan@gmail.com>
2018-10-30 21:55:35 +0300

    import category, product, sale views in applucation factory [#161533093]

--- app/__init__.py
+++ app/__init__.py
@@ -8,9 +8,16 @@ def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(app_config[config_name])
     jwt = JWTManager(app)
+
     from app.views.user import user as user_blueprint
+    from app.views.category import category as category_blueprint
+    from app.views.product import product as product_blueprint
+    from app.views.sale import sale as sale_blueprint
 
     app.register_blueprint(user_blueprint)
+    app.register_blueprint(category_blueprint)
+    app.register_blueprint(product_blueprint)
+    app.register_blueprint(sale_blueprint)
 
     return app
 
