56e749d55e00290166e4c2a3228bfdb18ac10975
faraqhan <f.faraqhan@gmail.com>
2018-10-31 04:47:43 +0300

    add endpoint for adding products

--- app/views/product.py
+++ app/views/product.py
@@ -39,6 +39,35 @@ def create_product(category_id):
     return jsonify({'message': f'{name}'
                                f' has successfully been added to inventories'}), 201
 
+
+@product.route("/api/v1/categories/<int:category_id>/products/<int:product_id>", methods=["PUT"])
+@jwt_required
+def modify_product(category_id, product_id):
+    """A method adds product instance to products list"""
+    db = Database(app.config['DATABASE_URI'])
+    if request.content_type != "application/json":
+        raise InvalidUsage("Invalid content type", 400)
+    current_user_id = get_jwt_identity()
+    user = db.find_user_by_id(current_user_id)
+    if user.is_admin is False:
+        raise InvalidUsage("you do not have admin rights", 403)
+    data = request.json
+    name = data.get("product_name")
+    price = data.get("product_price")
+    quantity = data.get("quantity")
+    category = db.find_category_by_category_id(category_id)
+    if not category:
+        raise InvalidUsage("category does not exist")
+    validate = v.validate(data, product_schema)
+    if not validate:
+        raise InvalidUsage({'error': v.errors}, 400)
+    found = db.find_product_by_product_name(name)
+    if found:
+        raise InvalidUsage(f"{found.product_name} name already exists", 400)
+    db.modify_product(name, quantity, price, product_id)
+    return jsonify({'message': f'{name} has successfully been modified'}), 200
+
+
 @product.errorhandler(InvalidUsage)
 def handle_invalid_usage(error):
     response = jsonify(error.to_dict())
