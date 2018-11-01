5f6a694cdbecee72c0f71455d3399d522d78f350
fahadmak <f.faraqhan@gmail.com>
2018-11-01 19:16:45 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ class Config(object):
     DEBUG = False
     CSRF_ENABLED = True
     JWT_SECRET_KEY = "bigsecret"
-    DATABASE_URI = 'postgres://tquvntrciomlye:0e9dfeca2f405e0f2193a3b7fbc6f316dd4f63caad0fdf9b5a4406bcb2fbc585@ec2-23-21-171-249.compute-1.amazonaws.com:5432/d1akcs0pg5u19u'
+    DATABASE_URI = 'postgresql://postgres:maka1997@localhost/testdb'
     JWT_ACCESS_TOKEN_EXPIRES = False
 
 
@@ -23,6 +23,7 @@ class ProductionConfig(Config):
     """Configurations for Production."""
     DEBUG = False
     TESTING = False
+    DATABASE_URI = 'postgres://tquvntrciomlye:0e9dfeca2f405e0f2193a3b7fbc6f316dd4f63caad0fdf9b5a4406bcb2fbc585@ec2-23-21-171-249.compute-1.amazonaws.com:5432/d1akcs0pg5u19u'
 
 
 app_config = {
