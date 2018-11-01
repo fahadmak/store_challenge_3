bae25edf1e435e09e94d4b7b021730fef77e85d7
faraqhan <f.faraqhan@gmail.com>
2018-11-01 17:47:25 +0300

    fix get all sales bug + json input bug

--- app/validation_schema.py
+++ app/validation_schema.py
@@ -1,21 +1,24 @@
+import string
 
-user_schema = {'name': {'required': True, 'min': 6, 'type': 'string'},
-               'username': {'type': 'string', 'min': 7, 'required': True},
-               'password': {'type': 'string', 'required': True}}
+letters = tuple(string.ascii_letters)
 
-login_schema = {'username': {'type': 'string', 'min': 7, 'required': True},
-                'password': {'type': 'string', 'required': True}}
+user_schema = {'name': {'required': True, 'min': 6, 'type': 'string', 'forbidden': [' ', "", "    "]},
+               'username': {'type': 'string', 'min': 7, 'required': True, 'forbidden': [' ', "", "    "]},
+               'password': {'type': 'string', 'required': True, 'forbidden': [' ', "", "    "]}}
 
-product_schema = {'product_name': {'type': 'string', 'required': True},
-                  'quantity': {'type': 'integer', 'required': True},
-                  'product_price': {'type': 'integer', 'required': True}}
+login_schema = {'username': {'type': 'string', 'min': 7, 'required': True, 'forbidden': [' ', "", "      "]},
+                'password': {'type': 'string', 'required': True, 'forbidden': [' ', "", "    "]}}
 
-add_product_schema = {'product_name': {'type': 'string', 'required': True},
-                      'quantity': {'type': 'integer', 'required': True},
-                      'product_price': {'type': 'integer', 'required': True},
-                      'category_id': {'type': 'integer', 'required': True}}
+product_schema = {'product_name': {'type': 'string', 'required': True, 'forbidden': [' ', "", "   "]},
+                  'quantity': {'type': 'integer', 'required': True, 'forbidden': [' ', ""]},
+                  'product_price': {'type': 'integer', 'required': True, 'forbidden': [' ', ""]}}
 
-category_schema = {'category_name': {'type': 'string', 'required': True}}
+add_product_schema = {'product_name': {'type': 'string', 'required': True, 'forbidden': [' ', "", "    "]},
+                      'quantity': {'type': 'integer', 'required': True, 'forbidden': [' ', "", "    "]},
+                      'product_price': {'type': 'integer', 'required': True, 'forbidden': [' ', "", "    "]},
+                      'category_id': {'type': 'integer', 'required': True, 'forbidden': [' ', "", "    "]}}
 
-sale_schema = {'quantity': {'type': 'integer', 'required': True},
-               'product_id': {'type': 'integer', 'required': True}}
+category_schema = {'category_name': {'type': 'string', 'required': True, 'forbidden': [' ', "", "    "]}}
+
+sale_schema = {'quantity': {'type': 'integer', 'required': True, 'empty': True},
+               'product_id': {'type': 'integer', 'required': True, 'empty': True}}
--- app/views/sale.py
+++ app/views/sale.py
@@ -25,27 +25,27 @@ def create_sale():
     user = db.find_user_by_id(current_user_id)
     if user.is_admin is False:
         raise InvalidUsage("you do not have admin rights", 401)
-
     data = request.json
-    product_id = data.get("product_id")
-    quantity = data.get("quantity")
-
-    validate = v.validate(data, sale_schema)
-    if not validate:
-        raise InvalidUsage({'error': v.errors}, 400)
-
-    product = db.find_product_by_product_id(product_id)
-    if not product:
-        raise InvalidUsage("product does not exist", 400)
-
-    if quantity < product.quantity:
-        total = product.price * quantity
-        new_stock = product.quantity - quantity
-        db.update_stock(new_stock, product_id)
-        db.add_sale(total, current_user_id)
-        return jsonify({'message': 'Sale record has been created successfully'}), 200
-
-    raise InvalidUsage("This quantity is unavailable, try lesser quantity", 400)
+    cart = data.get("cart")
+    total = 0
+    sales_by_id = db.get_max_sale_id()
+    sale_id = max([sold for sold in sales_by_id]) + 1 if sales_by_id else 1
+    for item in cart:
+        validate = v.validate(item, sale_schema)
+        if not validate:
+            raise InvalidUsage({'error': v.errors}, 400)
+        product = db.find_product_by_product_id(item['product_id'])
+        if not product:
+            raise InvalidUsage("product does not exist", 400)
+        if item['quantity'] < product.quantity:
+            raise InvalidUsage(f"This quantity of {product.product_name} ordered should less than "
+                               f"{item['quantity']}", 400)
+        total += product.price * item['quantity']
+        new_stock = product.quantity - item['quantity']
+        db.update_stock(new_stock, item['product_id'])
+        db.add_sold_item(item['quantity'], item['product_id'], sale_id)
+    db.add_sale(total, current_user_id, sale_id)
+    return jsonify({'message': 'Sale record has been created successfully'}), 200
 
 
 @sale.route("/api/v1/sales/<int:sale_id>", methods=["GET"])
@@ -59,6 +59,9 @@ def get_sale(sale_id):
     current_user_id = get_jwt_identity()
 
     db = Database(app.config['DATABASE_URI'])
+    user = db.find_user_by_id(current_user_id)
+    if user.is_admin is True:
+        raise InvalidUsage("You can't access this sale record", 401)
 
     item = db.find_sale_by_sale_id(sale_id)
     if item.user_id != current_user_id:
