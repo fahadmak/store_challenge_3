3ffce6380b73e5f0afa20334d50097197e29faf2
fahadmak <f.faraqhan@gmail.com>
2018-11-01 18:20:25 +0300

    Update test_config.py

--- tests/test_config.py
+++ tests/test_config.py
@@ -8,7 +8,7 @@ class TestDevelopmentConfig(unittest.TestCase):
         app = create_app("development")
         self.assertTrue(app.config['DEBUG'] is True)
         self.assertEqual(
-            app.config['DATABASE_URI'], 'postgresql://postgres:maka1997@localhost/store'
+            app.config['DATABASE_URI'], 'postgres://tquvntrciomlye:0e9dfeca2f405e0f2193a3b7fbc6f316dd4f63caad0fdf9b5a4406bcb2fbc585@ec2-23-21-171-249.compute-1.amazonaws.com:5432/d1akcs0pg5u19u'
         )
         self.assertTrue(app.config['JWT_SECRET_KEY'] == 'bigsecret')
 
