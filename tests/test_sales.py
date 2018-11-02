7aa0e9481f456fbbb038f6e4baa2501ee25592f4
faraqhan <f.faraqhan@gmail.com>
2018-11-02 10:58:49 +0300

    change functionality of add sale method + addd tests for amin trying to get a particular sale

--- tests/test_sales.py
+++ tests/test_sales.py
@@ -15,18 +15,24 @@ class TestSales(unittest.TestCase):
         self.token = json.loads(response1.data)['access_token']
 
     def test_create_sale(self):
+        post_signup = dict(name="Fahad", username="Giga", password="Shoort")
+        response = self.app.post('/api/v1/auth/signup', json=post_signup,
+                                 headers={'Authorization': 'Bearer ' + self.token})
+        login = dict(username="Giga", password="Shoort")
+        response1 = self.app.post('/api/v1/auth/login', json=login)
+        token = json.loads(response1.data)['access_token']
         post_add = dict(category_name="phill")
         response1 = self.app.post('/api/v1/categories', json=post_add,
-                                 headers={'Authorization': 'Bearer ' + self.token})
+                                 headers={'Authorization': 'Bearer ' + token})
         post_add2 = dict(product_name="Fahad2344", quantity=12, product_price=12, category_id=1)
         response2 = self.app.post('/api/v1/products', json=post_add2,
-                                  headers={'Authorization': 'Bearer ' + self.token})
+                                  headers={'Authorization': 'Bearer ' + token})
         sale = dict(product_id=1, quantity=10)
-        response = self.app.post('/api/v1/sales', json=sale,
-                                 headers={'Authorization': 'Bearer ' + self.token})
-        assert json.loads(response.data)['message'] == 'Sale record has been created successfully'
-        assert response.status_code == 200
-        assert response.headers["Content-Type"] == "application/json"
+        response4 = self.app.post('/api/v1/sales', json=sale,
+                                 headers={'Authorization': 'Bearer ' + token})
+        assert json.loads(response4.data)['error'] == 'you should be a sale attendant'
+        assert response4.status_code == 401
+        assert response4.headers["Content-Type"] == "application/json"
 
     def test_create_sale_low_quantity(self):
         post_add = dict(category_name="phill")
@@ -82,23 +88,23 @@ class TestSales(unittest.TestCase):
                                  headers={'Authorization': 'Bearer ' + self.token})
         response3 = self.app.get('/api/v1/sales/1', content_type='application/json',
                                  headers={'Authorization': 'Bearer ' + self.token})
-        assert json.loads(response3.data)['error'] == "You can't access this sale record"
-        assert response3.status_code == 401
+        assert json.loads(response3.data)['sale']['sale_id'] == 1
+        assert response3.status_code == 200
         assert response3.headers["Content-Type"] == "application/json"
 
     def test_get_all_sale(self):
 
         response3 = self.app.get('/api/v1/sales', content_type='application/json',
                                  headers={'Authorization': 'Bearer ' + self.token})
-        assert json.loads(response3.data) == {'error': 'No sale records'}
-        assert response3.status_code == 400
+        assert json.loads(response3.data) == {'error': 'you do not have admin rights'}
+        assert response3.status_code == 401
         assert response3.headers["Content-Type"] == "application/json"
 
     def test_no_sales(self):
         response3 = self.app.get('/api/v1/sales', content_type='application/json',
                                    headers={'Authorization': 'Bearer ' + self.token})
-        assert json.loads(response3.data) == {'error': 'No sale records'}
-        assert response3.status_code == 400
+        assert json.loads(response3.data) == {'error': 'you do not have admin rights'}
+        assert response3.status_code == 401
         assert response3.headers["Content-Type"] == "application/json"
 
     def tearDown(self):
