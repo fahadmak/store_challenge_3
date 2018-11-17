36ba489fbd44cb622018adbd6c3ba4f567c403bb
faraqhan <f.faraqhan@gmail.com>
2018-11-17 03:03:47 +0300

    change name of parameter name in product schema for validation

--- app/validation_schema.py
+++ app/validation_schema.py
@@ -6,16 +6,16 @@ user_schema = {'name': {'required': True, 'min': 6, 'type': 'string', 'forbidden
                'username': {'type': 'string', 'min': 7, 'required': True, 'forbidden': [' ', "", "    "]},
                'password': {'type': 'string', 'required': True, 'forbidden': [' ', "", "    "]}}
 
-login_schema = {'username': {'type': 'string', 'min': 7, 'required': True, 'forbidden': [' ', "", "      "]},
-                'password': {'type': 'string', 'required': True, 'forbidden': [' ', "", "    "]}}
+login_schema = {'username': {'type': 'string', 'required': True, 'regex': '^[a-z0-9_@]{5,15}$'},
+                'password': {'type': 'string', 'required': True, 'regex': '^[a-z0-9_@]{5,15}$'}}
 
-product_schema = {'product_name': {'type': 'string', 'required': True, 'forbidden': [' ', "", "   "]},
-                  'quantity': {'type': 'integer', 'required': True, 'forbidden': [' ', ""]},
-                  'product_price': {'type': 'integer', 'required': True, 'forbidden': [' ', ""]}}
+product_schema = {'name': {'type': 'string', 'required': True, 'regex': '^[A-Za-z0-9]+$'},
+                  'quantity': {'type': 'number', 'min': 1, 'required': True, 'forbidden': [' ', ""]},
+                  'price': {'type': 'number', 'min': 1, 'required': True, 'forbidden': [' ', ""]}}
 
-add_product_schema = {'product_name': {'type': 'string', 'required': True, 'forbidden': [' ', "", "    "]},
+add_product_schema = {'name': {'type': 'string', 'required': True, 'regex': '^[A-Za-z0-9]+$'},
                       'quantity': {'type': 'integer', 'required': True, 'forbidden': [' ', "", "    "]},
-                      'product_price': {'type': 'integer', 'required': True, 'forbidden': [' ', "", "    "]},
+                      'price': {'type': 'integer', 'required': True, 'forbidden': [' ', "", "    "]},
                       'category_id': {'type': 'integer', 'required': True, 'forbidden': [' ', "", "    "]}}
 
 category_schema = {'category_name': {'type': 'string', 'required': True, 'forbidden': [' ', "", "    "]}}
