c02d9e23954e72298d8859550a1676f4f78bcc00
fahadmak <fahad.makabugo@andela.com>
2019-03-03 05:43:06 +0300

    Update validation_schema.py

--- app/validation_schema.py
+++ app/validation_schema.py
@@ -3,11 +3,11 @@ import string
 letters = tuple(string.ascii_letters)
 
 user_schema = {'name': {'required': True, 'min': 6, 'type': 'string', 'regex': '^[A-Za-z0-9]+$'},
-               'username': {'type': 'string', 'min': 7, 'required': True, 'regex': '^[a-z0-9]{5,15}$'},
+               'username': {'type': 'string', 'min': 7, 'required': True, 'regex': '^[A-Za-z0-9]{5,15}$'},
                'password': {'type': 'string', 'required': True, 'regex': '^[a-z0-9]{5,15}$'}}
 
-login_schema = {'username': {'type': 'string', 'required': True, 'regex': '^[a-z0-9_@]{5,15}$'},
-                'password': {'type': 'string', 'required': True, 'regex': '^[a-z0-9_@]{5,15}$'}}
+login_schema = {'username': {'type': 'string', 'required': True, 'regex': '^[A-Za-z0-9_@]{5,15}$'},
+                'password': {'type': 'string', 'required': True, 'regex': '^[A-Za-z0-9_@]{5,15}$'}}
 
 product_schema = {'name': {'type': 'string', 'required': True, 'regex': '^[A-Za-z0-9]+$'},
                   'quantity': {'type': 'number', 'min': 1, 'required': True, 'forbidden': [' ', ""]},
